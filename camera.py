d202553c7d11ee6cc32b79cd888717e3977703f3
yaroslavorl <jaroslavorlenko@yandex.ru>
2023-06-18 18:33:32 +0300

    new camera class

--- camera.py
+++ camera.py
@@ -1,72 +1,83 @@
 import torch
 import cv2
-import time
 
 
-def detector(x, pattern):
-    return torch.matmul(pattern, x)
+class Camera:
+
+    def __init__(self, size_img, pattern_num):
+
+        self.img = None
+        self.size_img = size_img
+        self.pattern_num = pattern_num
+        self.loss = None
+        self.optimizer = None
+
+    def detector(self, pattern, img):
+        return torch.matmul(pattern, img)
+
+    def dct_matrix(self):
+        n = self.size_img[0] * self.size_img[1]
+        matrix = torch.zeros((n, n))
+        for i in range(1, n):
+            for j in range(n):
+                matrix[i][j] = (2 * j + 1) * i * torch.pi / (2 * n)
+        matrix = torch.sqrt(torch.tensor(2 / n)) * torch.cos(matrix)
+        matrix[0] = 1 / torch.sqrt(torch.tensor(n))
+
+        return matrix
+
+    def binary_mask(self):
+        return 2 * torch.randint(0, 2, size=(self.pattern_num, self.size_img[0] * self.size_img[1]),
+                                 dtype=torch.float32) - 1
+
+    def get_img(self):
+        return self.img.detach().clone()
+
+    def lasso(self, w, alpha=0.1):
+        return alpha * torch.sum(torch.abs(w))
+
+    def compile(self, img, loss, optimizer, lr):
+        self.img = img
+        self.loss = loss()
+        self.optimizer = optimizer([self.img], lr=lr)
+
+    def fit(self, basis, y, epochs):
+
+        for epoch in range(1, epochs + 1):
+            output = torch.matmul(basis, self.img)
+            error = self.loss(output, y) + self.lasso(self.img)
+
+            self.optimizer.zero_grad()
+            error.backward()
+            self.optimizer.step()
+
+            print(f'Эпоха: {epoch}/{epochs}, loss: {error.item()}')
 
 
-def dct_matrix(n):
-    matrix = torch.zeros((n, n))
-
-    for i in range(1, n):
-        for j in range(n):
-            matrix[i][j] = (2 * j + 1) * i * torch.pi / (2 * n)
-
-    matrix = torch.sqrt(torch.tensor(2 / n)) * torch.cos(matrix)
-    matrix[0] = 1 / torch.sqrt(torch.tensor(n))
-
-    return matrix
-
-
-def binary_mask(size):
-    return 2 * torch.randint(0, 2, size, dtype=torch.float32) - 1
-
-
-def lasso(w, alpha=0.1):
-    return alpha * torch.sum(torch.abs(w))
-
-
-def grad_descent(img, img_y, p, loss, optimizer, epochs):
-
-    for epoch in range(1, epochs + 1):
-
-        output = torch.matmul(p, img)
-        error = loss(output, img_y) + lasso(img)
-
-        optimizer.zero_grad()
-        error.backward()
-        optimizer.step()
-
-        print(f'Эпоха: {epoch}/{epochs}, loss: {error.item()}')
-
-    return img
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     pattern_num = 300
-
-    img = cv2.imread('lena.jpg', 0)
+    img = cv2.imread('test.jpg', 0)
     img = torch.FloatTensor(img) / 255
     size_img = img.shape
 
-    basis = dct_matrix(size_img[0] * size_img[1])
-    pattern = binary_mask(size=(pattern_num, size_img[0] * size_img[1]))
-    y = detector(img.flatten(), pattern)
+    model = Camera(size_img=size_img, pattern_num=pattern_num)
+
+    basis = model.dct_matrix()
+    pattern = model.binary_mask()
+    y = model.detector(pattern, img.flatten())
 
     img = torch.randint(0, 2, (size_img[0], size_img[1]), dtype=torch.float32)
+
     img = torch.matmul(basis, img.flatten())
     basis = basis.T
     p = torch.matmul(pattern, basis)
 
     img.requires_grad = True
-    optimizer = torch.optim.Adam([img], lr=0.03)
-    loss = torch.nn.MSELoss()
+    model.compile(img, loss=torch.nn.MSELoss, optimizer=torch.optim.Adam, lr=0.03)
+    model.fit(p, y, epochs=500)
 
-    start = time.time()
-    img = grad_descent(img, y, p, loss, optimizer, epochs=500)
-    print(time.time() - start)
+    img = model.get_img()
 
-    cv2.imwrite('test.jpg', torch.matmul(basis, img).reshape(size_img).detach().numpy() * 255)
+    cv2.imwrite('test_2.jpg',
+                torch.matmul(basis, img).reshape(size_img).numpy() * 255)
